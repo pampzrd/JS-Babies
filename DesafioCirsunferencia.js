2c86ee383ec0d77df17081b0972ce906ce1bd90c
Pamela Rod <akahime2014@gmail.com>
2022-01-20 03:00:40 -0300

    upload

---
+++ DesafioCirsunferencia.js
@@ -0,0 +1,13 @@
+// Calcular a circunferência
+// PI * raio * raio
+// PI = 3,141592
+
+const PI = 3.141592
+let raio = 10
+let areaC = (raio*raio)*PI
+
+console.log("O valor da circunferência do raio de "+raio+" é "+areaC+"m².")
+
+//console.log(Math.PI)  Pega o valor de PI no JS
+//Poderia ser Math.PI*raio*raio
+//dar preferência para variáveis constantes, a menos que seja necessário variar o valor depois
---
+++ dados.js
@@ -0,0 +1,35 @@
+//aula 23 Trabalhando com dados
+
+/* console.log("Caneta");
+console.log(10);
+console.log(6.4);
+console.log(1.5);
+console.log(7.9); */
+
+//declarando variáveis
+
+/* var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var nome = "Caneta";
+
+console.log(nome); */
+/* console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(preco + imposto);
+
+// ou declara uma variavel separada
+var precoFinal = preco + imposto;
+console.log(precoFinal);
+
+nome = "Caneta BIC";
+console.log(nome); //atualiza a variavel nome */
+
+/* let frase = "Banda de hoje";
+let banda = ["Dead by April","Paramore","My Chemical Romance","Shaman","Angra","Evanescence",
+"Fresno","Glória","Linkin Park","Metallica","YOHIO","Girugamesh","KLB"];
+
+console.log(frase);
+ */
+
